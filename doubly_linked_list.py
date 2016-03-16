5c6bf19cd8dc01b35062bb1937c2b03c1a4bf08d
Mike Kane <mike.kane@students.makeschool.com>
2016-03-16 12:41:34 +0000

    added unit tests

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -50,3 +50,53 @@ class DoublyLinkedList():
 
     def deleteNodeWithData(self, data):
         ...
+
+class TestDoublyLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.dll = DoublyLinkedList()
+
+    def test_insert_at_head(self):
+        self.dll.insertAtHead(1)
+        self.dll.insertAtHead(0) # 0, 1
+        self.assertEqual(0, self.dll.head.data)
+        self.assertEqual(1, self.dll.tail.data)
+
+    def test_insert_at_tail(self):
+        self.dll.insertAtTail(1)
+        self.dll.insertAtTail(2) # 1, 2
+        self.assertEqual(1, self.dll.head.data)
+        self.assertEqual(2, self.dll.tail.data)
+
+    def test_index_for_data(self):
+        self.dll.insertAtTail(1)
+        self.dll.insertAtTail(2)
+        self.dll.insertAtTail(3)  # 1, 2, 3
+        index = self.dll.getIndexForData(2)
+        self.assertEqual(index, 1)
+
+    def test_get_data_at_index(self):
+        self.dll.insertAtTail(1)
+        self.dll.insertAtTail(2)
+        self.dll.insertAtTail(3)  # 1, 2, 3
+        dataAtIndex = self.dll.getDataAtIndex(1)
+        self.assertEqual(dataAtIndex, 2)
+
+    def test_delete_data_at_index(self):
+        self.dll.insertAtTail(1)
+        self.dll.insertAtTail(2)
+        self.dll.insertAtTail(3)      # 1, 2, 3
+        self.dll.deleteNodeAtIndex(1) # 1, 3
+        index = self.dll.getIndexForData(3)
+        self.assertEqual(index, 1)
+
+    def test_delete_node_with_data(self):
+        self.dll.insertAtTail(1)
+        self.dll.insertAtTail(2)
+        self.dll.insertAtTail(3)      # 1, 2, 3
+        self.dll.deleteNodeWithData(2)
+        results = self.dll.getIndexForData(2)
+        self.assertEqual("data not in list", results)
+
+if __name__ == "__main__":
+    unittest.main()
